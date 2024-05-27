e1014a6a75cc977dc2e79e980a098b75464dbc9d
Your Name <you@example.com>
2024-05-27 10:14:37 +0300

    add pring matrix

--- src/Main.cpp
+++ src/Main.cpp
@@ -21,10 +21,10 @@ int sumOfAllNumber(double matrix[ROW_SIZE][COLUMN_SIZE])
 void enterRow(double matrix[ROW_SIZE][COLUMN_SIZE], int rowNumber)
 {
 	assert(rowNumber < ROW_SIZE && ROW_SIZE > 0);
+	assert(COLUMN_SIZE> 0);
 	auto input = 0;
-	auto isInputComplete = false;
 	int completedInputs = 0;
-	while (!isInputComplete)
+	while (completedInputs != COLUMN_SIZE)
 	{
 		std::cin >> input;
 		if (std::cin.fail())
@@ -32,16 +32,34 @@ void enterRow(double matrix[ROW_SIZE][COLUMN_SIZE], int rowNumber)
 			std::cout << "Invalid input try again\n";
 			continue;
 		}
-		if (abs(input )> )
+		if (abs(input) > 2.5)
+		{
+			std::cout << "Input must be in range (-2.5,2.5)\n";
+			continue;
+		}
+		matrix[rowNumber][completedInputs++] = input;
 	}
 	
+}
+
+void printMatrix(double matrix[ROW_SIZE][COLUMN_SIZE])
+{
+	assert(COLUMN_SIZE > 0 && ROW_SIZE > 0);
+	for (int i = 0; i < ROW_SIZE; i++)
+	{
+		for (int j = 0; j < COLUMN_SIZE; j++)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
 
 int main()
 {
 	
 	auto isRunning = true;
-	double matrix[4][3];
+	double matrix[4][3] = {0,0,0,   0,0,0, 0,0,0, 0,0,0 };
 	while (isRunning)
 	{
 		std::cout << "1) Enter data in matrix (range -2.5 to 2.5)\n";
@@ -53,16 +71,23 @@ int main()
 		switch (choice)
 		{
 		case 1:
-			break;
-		case 2: 
-			break;
-		case 3:
-			break;
-		case 4:
 		{
-			isRunning = false;
+			for (int i = 0; i < COLUMN_SIZE; i++)
+			{
+				std::cout << "Enter " << i << "row "<< std::endl;
+				enterRow(matrix, i);
+			}
 			break;
 		}
+		case 2: 
+			auto sum = sumOfAllNumber(matrix);
+			break;
+		case 3:
+			printMatrix(matrix);
+			break;
+		case 4:
+			isRunning = false;
+			break;
 		default:
 		{
 			std::cout << "Wrong input\n";
